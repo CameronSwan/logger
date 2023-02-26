ff7da53567e92cdae9a2f8134b1e6e22f6263af1
Maya <60881869+mayannamarie@users.noreply.github.com>
2023-02-26 02:55:00 -0400

    Initial User model complete

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,48 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema ({
-
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        min: [5,'Must be at least 5'],
+        max: 20
+    },
+    password: {
+        type: String,
+        required: true,
+        min: [7,'Must be at least 7'],
+        max: 20
+    },
+    phoneNumber: {
+        type: Number,
+        required: [true, 'Phone number is Required']
+    },
+    roleId: {
+        type: Number,
+        required: [true, 'Role ID is Required']
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    verifiedBy: {
+        type: Number,
+        required: true
+    },
+    verifiedAt: {
+        type: Date,
+        required: [true, 'verifiedAt is Required.']
+    },
+    createdBy: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: [true, 'createdAt is Required.']
+    }
 });
 
 
