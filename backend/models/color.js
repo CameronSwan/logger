c103953c5fe4202e1eb2bcaa3b073742ec21e352
Maya <60881869+mayannamarie@users.noreply.github.com>
2023-02-27 19:23:51 -0400

    ID removed

--- backend/models/color.js
+++ backend/models/color.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const colorSchema = new Schema ({
-    colorId: {
-        type: Number,
-        required: [true, 'Color ID is Required']
-    },
+
     hexCode: {
         type: String,
         required: [true, 'Color HexCode is Required']
