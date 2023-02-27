00252998e8dae48b4d4cad74e22eb8e5cf090f3f
Maya <60881869+mayannamarie@users.noreply.github.com>
2023-02-27 19:09:05 -0400

    ID removed

--- backend/models/post.js
+++ backend/models/post.js
@@ -3,10 +3,6 @@ const { Schema } = mongoose;
 
 const postSchema = new Schema ({
 
-    postId: {
-        type: Number,
-        required: [true, 'Post ID is Required']
-    },
     title: {
         type: String,
         required: [true, 'Post Name is Required']
