3a45b41ed6425a3999e93a4151d482a98f97b032
Maya <60881869+mayannamarie@users.noreply.github.com>
2023-03-06 16:21:55 -0400

    Starter gets for symptoms

---
+++ backend/routes/api/symptoms.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const Symptom = require('../../models/symptom')
+
+// Return All Symptoms
+router.get('/', (req, res) => {
+    Symptom.find({}, (e, symptoms) => {
+        if (e) res.status(400).send()
+        if (symptoms) res.status(200).json(symptoms)
+        else res.status(404).send()
+    })
+})
+
+// Return Symptom With Provided _id
+router.get('/:id', (req, res) => {
+    Symptom.findById(req.params.id, (e, symptom) => {
+        if (e) res.status(400).send()
+        if (symptom) res.status(200).json(symptom)
+        else res.status(404).send()
+    })
+})
+
+module.exports = router
