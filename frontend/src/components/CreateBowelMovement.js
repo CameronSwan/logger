e2195114afcd2e6867f46c513e1edd09f757f0ce
Christina Hemeon <christinahemeon@gmail.com>
2023-03-13 21:34:43 -0300

    Stop info buttons from submitting form, fix table issue, add classnames

--- frontend/src/components/CreateBowelMovement.js
+++ frontend/src/components/CreateBowelMovement.js
@@ -87,7 +87,7 @@ const CreateBowelMovement = () => {
 
         //If time selected is later than current time, do not allow submission
         if (new Date(`${date} ${time}`) > new Date()) {
-            setDateTimeWarning("Cannot select future occurence.");
+            setDateTimeWarning('Cannot select future occurence.');
         } else {
             dataService.createBowelMovement({
                 date: date,
@@ -107,70 +107,83 @@ const CreateBowelMovement = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <h1>New Entry</h1>
+        <div className='form__wrapper'>
+            <form className='form' onSubmit={handleSubmit}>
+                <h1 className='form__title'>New Entry</h1>
 
-                <div>
-                    <div>
-                        <label htmlFor="date">
-                            Date
-                        </label>
-                        <input type="date" id="datePicker" name="date"
-                            max={new Date().toLocaleString('sv').split(' ')[0]}
-                            value={date}
-                            onChange={e => setDate(e.target.value)} />
-                    </div>
-
-                    <div>
-                        <label htmlFor="time">
-                            Time
-                        </label>
-                        <input type="time" id="timePicker" name="time"
-                            value={time}
-                            onChange={e => setTime(e.target.value)}
-                            step="60" />
-                        <div>
-                            {
-                                errors.date && <span>{errors.date.message}</span>
-                            }
-                            {
-                                errors.time && <span>{errors.time.message}</span>
-                            }
-                            {
-                                dateTimeWarning && <span>{dateTimeWarning}</span>
-                            }
+                <div className='form__row'>
+                    <div className='form__row--split'>
+                        <div className='form__half-row'>
+                            <label htmlFor="datePicker">
+                                Date
+                            </label>
+                            <div>
+                                <input type='date' id='datePicker' name='date'
+                                    max={new Date().toLocaleString('sv').split(' ')[0]}
+                                    value={date}
+                                    onChange={e => setDate(e.target.value)}
+                                    className='form__text-input' />
+                            </div>
                         </div>
+
+                        <div className='form__half-row'>
+                            <label htmlFor='timePicker'>
+                                Time
+                            </label>
+                            <div>
+                                <input type='time' id='timePicker' name='time'
+                                    value={time}
+                                    onChange={e => setTime(e.target.value)}
+                                    step='60'
+                                    className='form__text-input' />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='form__error'>
+                        {
+                            errors.date && <span className='error'>{errors.date.message}</span>
+                        }
+                        {
+                            errors.time && <span className='error'>{errors.time.message}</span>
+                        }
+                        {
+                            dateTimeWarning && <span className='error'>{dateTimeWarning}</span>
+                        }
                     </div>
                 </div>
 
-                <div>
+                <div className='form__row'>
                     <Modal
+                        key='stoolTypeModal'
                         isOpen={stoolTypeModalIsOpen}
                         onRequestClose={() => setStoolTypeModalIsOpen(false)}
                         style={customStyles}
-                        contentLabel="Bristol Stool Chart Details"
+                        contentLabel='Bristol Stool Chart Details'
                     >
-                        <div className="modal__header">
-                            <h2>Bistol Stool Scale</h2>
-                            <button onClick={() => setStoolTypeModalIsOpen(false)} aria-label="Close" title="Close">X</button>
+                        <div className='modal__header'>
+                            <h2 className='modal__title'>Bistol Stool Scale</h2>
+                            <button onClick={() => setStoolTypeModalIsOpen(false)} aria-label='Close' title='Close'>X</button>
                         </div>
-                        <table className="modal__table modal__table--stooltypes">
+                        <table className='modal__table modal__table--stooltypes'>
+                            <tbody>
                             {stoolTypes.map(stoolType => {
                                 return (
-                                    <tr>
-                                        <td>image</td> <td>{stoolType.name}</td> <td>{stoolType.description}</td>
+                                    <tr key={stoolType.name}>
+                                        <td>image</td>
+                                        <td>{stoolType.name}</td>
+                                        <td>{stoolType.description}</td>
                                     </tr>
                                 )
                             })}
+                            </tbody>
                         </table>
                         <div>Learn More</div>
                     </Modal>
 
                     <fieldset>
                         <legend>
-                            <span className="label">Bristol Stool Scale</span>
-                            <button onClick={() => setStoolTypeModalIsOpen(true)} aria-label="Open Bristol Stool Scale Details" title="Bristol Stool Scale Details">?</button>
+                            <span className='label'>Bristol Stool Scale</span>
+                            <button onClick={() => setStoolTypeModalIsOpen(true)} type='button' aria-label='Open Bristol Stool Scale Details' title='Bristol Stool Scale Details' className='button--info'>?</button>
                         </legend>
                         {
                             stoolTypes.map(stoolType => {
@@ -180,48 +193,53 @@ const CreateBowelMovement = () => {
                                         title={stoolType.name}
                                         key={stoolType.name}
                                         value={stoolType._id}
-                                        name="stoolTypesSelected"
-                                        className={"checkbox--stooltype"}
+                                        name='stoolTypesSelected'
+                                        className={'checkbox--stooltype'}
                                         onChange={handleSelection}
                                     />
                                 )
                             })
                         }
                     </fieldset>
-                    <div>
+                    <div className='form__error'>
                         {
-                            errors.stoolTypes && <span>{errors.stoolTypes.message}</span>
+                            errors.stoolTypes && <span className='error'>{errors.stoolTypes.message}</span>
                         }
                     </div>
                 </div>
 
-                <div>
+                <div className='form__row'>
                     <Modal
+                        key='colorModal'
                         isOpen={colorModalIsOpen}
                         onRequestClose={() => setColorModalIsOpen(false)}
                         style={customStyles}
-                        contentLabel="Color Details"
+                        contentLabel='Color Details'
                     >
-                        <div className="modal__header">
-                            <h2>Colors</h2>
-                            <button onClick={() => setColorModalIsOpen(false)} aria-label="Close" title="Close">X</button>
+                        <div className='modal__header'>
+                            <h2 className='modal__title'>Colors</h2>
+                            <button onClick={() => setColorModalIsOpen(false)} aria-label='Close' title='Close'>X</button>
                         </div>
-                        <table className="modal__table modal__table--colors">
+                        <table className='modal__table modal__table--colors'>
+                            <tbody>
                             {colors.map(color => {
                                 return (
-                                    <tr>
-                                        <td>image</td> <td>{color.name}</td>
+                                    <tr key={color.name}>
+                                        <td>image</td>
+                                        <td>{color.name}</td>
                                     </tr>
                                 )
                             })}
+                            </tbody>
                         </table>
                         <div>Learn More</div>
                     </Modal>
 
                     <fieldset>
                         <legend>
-                            <span className="label">Color</span>
-                            <button onClick={() => setColorModalIsOpen(true)} aria-label="Open Color Details" title="Color Details">?</button>
+                            <span className='label'>Color</span>
+                            <button onClick={() => setColorModalIsOpen(true)}
+                            type='button' aria-label='Open Color Details' title='Color Details' className='button--info'>?</button>
                         </legend>
                         {
                             colors.map(color => {
@@ -231,48 +249,52 @@ const CreateBowelMovement = () => {
                                         title={color.name}
                                         key={color.name}
                                         value={color._id}
-                                        name="colorsSelected"
-                                        className={"checkbox--color"}
+                                        name='colorsSelected'
+                                        className={'checkbox--color'}
                                         onChange={handleSelection}
                                     />
                                 )
                             })
                         }
                     </fieldset>
-                    <div>
+                    <div className='form__error'>
                         {
-                            errors.colors && <span>{errors.colors.message}</span>
+                            errors.colors && <span className='error'>{errors.colors.message}</span>
                         }
                     </div>
                 </div>
 
-                <div>
+                <div className='form__row'>
                     <Modal
+                        key='symptomModal'
                         isOpen={symptomModalIsOpen}
                         onRequestClose={() => setSymptomModalIsOpen(false)}
                         style={customStyles}
-                        contentLabel="Symptoms Details"
+                        contentLabel='Symptoms Details'
                     >
-                        <div className="modal__header">
-                            <h2>Symptoms Information</h2>
+                        <div className='modal__header'>
+                            <h2 className='modal__title'>Symptoms Information</h2>
                             <button onClick={() => setSymptomModalIsOpen(false)} aria-label="Close" title="Close">X</button>
                         </div>
-                        <table className="modal__table modal__table--symptoms">
-                            {symptoms.map(symptom => {
-                                return (
-                                    <tr>
-                                        <td>{symptom.name}</td> <td>{symptom.description}</td>
-                                    </tr>
-                                )
-                            })}
+                        <table className='modal__table modal__table--symptoms'>
+                            <tbody>
+                                {symptoms.map(symptom => {
+                                    return (
+                                        <tr key={symptom.name}>
+                                            <td>{symptom.name}</td>
+                                            <td>{symptom.description}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
                         </table>
                         <div>Learn More</div>
                     </Modal>
 
                     <fieldset>
                         <legend>
-                            <span className="label">Symptoms</span>
-                            <button onClick={() => setSymptomModalIsOpen(true)} aria-label="Open Symptom Details" title="Symptom Details">?</button>
+                            <span className='label'>Symptoms</span>
+                            <button onClick={() => setSymptomModalIsOpen(true)} type='button' aria-label='Open Symptom Details' title='Symptom Details' className='button--info'>?</button>
                         </legend>
                         {
                             symptoms.map(symptom => {
@@ -281,46 +303,51 @@ const CreateBowelMovement = () => {
                                         label={symptom.name}
                                         key={symptom.name}
                                         value={symptom._id}
-                                        name="symptomsSelected"
-                                        className={"checkbox--symptom"}
+                                        name='symptomsSelected'
+                                        className={'checkbox--symptom'}
                                         onChange={handleSelection}
                                     />
                                 )
                             })
                         }
                     </fieldset>
-                    <div>
+                    <div className='form__error'>
                         {
-                            errors.symptoms && <span>{errors.symptoms.message}</span>
+                            errors.symptoms && <span className='error'>{errors.symptoms.message}</span>
                         }
                     </div>
                 </div>
 
-                <div>
-                    <label htmlFor="inputNotes">
+                <div className='form__row'>
+                    <label htmlFor='inputNotes'>
                         Notes
                     </label>
-                    <textarea
-                        id="inputNotes"
-                        name="notes"
-                        onChange={e => setNotes(e.target.value)}
-                    />
                     <div>
+                        <textarea
+                            id='inputNotes'
+                            name='notes'
+                            onChange={e => setNotes(e.target.value)}
+                            className='form__textarea'
+                        />
+                    </div>
+                    <div className='form__error'>
                         {
-                            errors.notes && <span>{errors.notes.message}</span>
+                            errors.notes && <span className='error'>{errors.notes.message}</span>
                         }
                     </div>
                 </div>
 
-                <div>
+                <div className='form__error form__error--final'>
                     {
-                        errors.serverMessage && <span>{errors.serverMessage}</span>
+                        errors.serverMessage && <span className='error'>{errors.serverMessage}</span>
                     }
                 </div>
 
-                <button type="submit">
-                    Save
-                </button>
+                <div className='form__button-row'>
+                    <button type="submit" className='button button--submit cta'>
+                        Save
+                    </button>
+                </div>
 
             </form>
         </div>
