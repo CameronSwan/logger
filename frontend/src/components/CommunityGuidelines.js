b3d1776c606f5284bb60bc815f6eb58bfe3586db
Maya <60881869+mayannamarie@users.noreply.github.com>
2023-03-15 12:25:25 -0300

    More Formatting

--- frontend/src/components/CommunityGuidelines.js
+++ frontend/src/components/CommunityGuidelines.js
@@ -8,8 +8,6 @@ return (
         <div>
                 
             <h1>Community Guidelines</h1>
-            
-            <div>
                 
                 <h2>Poop Logger Community Guidelines and Anti-Harassment Policy</h2><br/>
                 <p>
@@ -19,9 +17,7 @@ return (
                     harassment in any form. Poop Logger takes violations of our policy seriously and will respond appropriately. 
                     All participants of Poop Logger, including online attendees, staff, and Poop Loggers, must abide by the following policy: 
                 </p>
-            </div>
-
-            <div>        
+    
                 <h3>Be respectful to each other.</h3>
                 <p>
                     Treat everyone with respect. Participate while acknowledging that everyone deserves to be here — and each of 
@@ -29,24 +25,17 @@ return (
                     blatant or via micro-aggressions. All forms of communication should not demean others. Consider what you are 
                     saying and how it would feel if it were said to you, or about you.
                 </p>
-            </div>
 
-
-            <div>   
                 <h3>Speak up if you see or hear something.</h3>
                 <p>
                     Harassment is not tolerated, and you are empowered to politely engage when you or others are disrespected. 
                     The person making you feel uncomfortable may not be aware of what they are doing, and politely bringing their 
                     behavior to their attention is encouraged.
                 </p>           
-            </div>
 
-            <div>
-                
-                <h2>
+                <h3>
                     We have a <strong>ZERO TOLERANCE POLICY</strong> for online harassment of any kind, including but not limited to:
-                </h2>
-                <p>
+                </h3>
                     <ul>
                         <li>Deliberate intimidation</li>
                         <li>Offensive language </li>
@@ -54,38 +43,34 @@ return (
                         <li>Cyber threats </li>
                         <li>Deliberate intimidation</li>
                     </ul>
-                </p>         
-            </div>
 
-            <div>
-                    <h2>
-                        In relation to, but not limited to:
-                    </h2>
-                    <ul>
-                        <li>Neurodiversity</li>
-                        <li>Race</li>
-                        <li>Color</li> 
-                        <li>National origin </li>
-                        <li>Gender identity </li>
-                        <li>Gender expression </li>
-                        <li>Sexual orientation </li>
-                        <li>Age</li>
-                        <li>Body size</li>
-                        <li>Disabilities </li>
-                        <li>Appearance </li>
-                        <li>Religion </li>
-                        <li>Pregnancy </li>
-                        <li>Military status </li>
-                        <li>Social demographic</li>
-                    </ul>          
-            </div>
+                <strong>In relation to, but not limited to:</strong>
+                <ul>
+                    <li>Neurodiversity</li>
+                    <li>Race</li>
+                    <li>Color</li> 
+                    <li>National origin </li>
+                    <li>Gender identity </li>
+                    <li>Gender expression </li>
+                    <li>Sexual orientation </li>
+                    <li>Age</li>
+                    <li>Body size</li>
+                    <li>Disabilities </li>
+                    <li>Appearance </li>
+                    <li>Religion </li>
+                    <li>Pregnancy </li>
+                    <li>Military status </li>
+                    <li>Social demographic</li>
+                </ul>          
 
-            <div>
                 <p>
                     Participants asked to stop any harassing behavior are expected to comply immediately. Our zero-tolerance 
                     policy means that we will look into and review every allegation of violation of our Community Guidelines 
                     and Anti-Harassment Policy and respond appropriately. to report any behavior that makes you or others 
-                    feel uncomfortable, send us a note at W0455841@nscc.ca. <br/>
+                    feel uncomfortable, send us a note at W0455841@nscc.ca. 
+                </p>
+
+                <p>
                     This policy extends to talks, forums, workshops, social media, partners, sponsors, staff, etc. 
                     You catch our drift. Poop Logger reserves the right to refuse admittance to, or remove any person from, 
                     any Poop Logger forum at any time in its sole discretion. This includes, but is not limited to, 
@@ -94,7 +79,6 @@ return (
                     take any action they deem appropriate, including warning or expelling the offender from the app with blocking 
                     the offender's account from participating online. 
                 </p>
-            </div>
         </div>    
     );
 }
