3852202a0be3724f35dc37a273d618f7ef2859f4
Christina Hemeon <christinahemeon@gmail.com>
2023-03-15 13:12:43 -0300

    start history tab styling

--- frontend/src/components/BowelMovement.js
+++ frontend/src/components/BowelMovement.js
@@ -5,14 +5,19 @@ import { Link } from "react-router-dom";
 export const BowelMovement = (props) => {
     const [stoolType, setStoolType] = useState('');
 
-    return (
-        <div>
-            <h4>{props.bm._id}</h4>
-            {props.bm.date}
-            {props.bm.time}
-            {props.bm.stoolTypes}
+    const displayTime = new Date(`${props.bm.date} ${props.bm.time}`).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
 
-            <div><Link to={`/bowelmovement/edit/${props.bm._id}`} relative="path" className="btn btn-sm btn-outline-secondary">edit</Link>
+    return (
+        <div className='bm-tab__wrapper'>
+            <div className='bm-tab__time h4'>
+                {displayTime}
+            </div>
+            <div className='bm-tab'>
+                {props.bm.stoolTypes}
+
+                <div>
+                    <Link to={`/bowelmovement/edit/${props.bm._id}`} relative="path" title='Edit'>edit</Link>
+                </div>
             </div>
 
         </div>
