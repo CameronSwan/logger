1c130235ad433311b05da872a4aebb821a074819
Maya <60881869+mayannamarie@users.noreply.github.com>
2023-03-15 10:55:09 -0300

    Added some formatting

--- frontend/src/components/CommunityGuidelines.js
+++ frontend/src/components/CommunityGuidelines.js
@@ -1,99 +1,103 @@
 import React from "react";
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const CommunityGuidelines = () => {
     const navigate = useNavigate();
 
 return (
-    <div>
-        
-        <h1>Community Guidelines</h1>
-
-        <h2>Poop Logger Community Guidelines and Anti-Harassment Policy </h2> 
-
         <div>
-            Poop Logger is dedicated to providing a harassment-free and inclusive experience for everyone regardless 
-            of gender identity and expression, sexual orientation, disabilities, neurodiversity, physical appearance, 
-            body size, ethnicity, nationality, race, age, religion, or another protected category. We do not tolerate 
-            harassment in any form. Poop Logger takes violations of our policy seriously and will respond appropriately. 
-            All participants of Poop Logger, including online attendees, staff, and Poop Loggers, must abide by the following policy: 
-            <br/>
-        </div>
-
-        <h3>
-            Be respectful to each other. 
-        </h3>
-
-        <div>
-            Treat everyone with respect. Participate while acknowledging that everyone deserves to be here — and each of 
-            us has the right to enjoy our experience without fear of harassment, discrimination, or condescension, whether 
-            blatant or via micro-aggressions. All forms of communication should not demean others. Consider what you are 
-            saying and how it would feel if it were said to you, or about you. <br/>
-        </div>
-        <h3>
-            Speak up if you see or hear something. 
-        </h3>
-
-        <div>
-            Harassment is not tolerated, and you are empowered to politely engage when you or others are disrespected. 
-            The person making you feel uncomfortable may not be aware of what they are doing, and politely bringing their 
-            behavior to their attention is encouraged. <br/>
             
-        </div>
+            <h1>Community Guidelines</h1>
+            
 
-        <div>
-            <h2>
-                We have a <strong>ZERO TOLERANCE POLICY</strong> for online harassment of any kind, including but not limited to:
-            </h2>
-            <ul>
-                <li>Deliberate intimidation</li>
-                <li>Offensive language </li>
-                <li>Language that reinforces social structures of domination</li>
-                <li>Cyber threats </li>
-                <li>Deliberate intimidation</li>
-            </ul>
-        </div>
+            <div>
+                <p>
+                    <h2><strong>Poop Logger Community Guidelines and Anti-Harassment Policy</strong></h2><br/>
+                    Poop Logger is dedicated to providing a harassment-free and inclusive experience for everyone regardless 
+                    of gender identity and expression, sexual orientation, disabilities, neurodiversity, physical appearance, 
+                    body size, ethnicity, nationality, race, age, religion, or another protected category. We do not tolerate 
+                    harassment in any form. Poop Logger takes violations of our policy seriously and will respond appropriately. 
+                    All participants of Poop Logger, including online attendees, staff, and Poop Loggers, must abide by the following policy: 
+                </p>
+            </div>
 
-        <div>
-            <h2>
-                In relation to, but not limited to:
-            </h2>
-            <ul>
-                <li>Neurodiversity</li>
-                <li>Race</li>
-                <li>Color</li> 
-                <li>National origin </li>
-                <li>Gender identity </li>
-                <li>Gender expression </li>
-                <li>Sexual orientation </li>
-                <li>Age</li>
-                <li>Body size</li>
-                <li>Disabilities </li>
-                <li>Appearance </li>
-                <li>Religion </li>
-                <li>Pregnancy </li>
-                <li>Military status </li>
-                <li>Social demographic</li>
-            </ul>
-        </div>
+            <div>
+                <p>
+                    <h3><strong>Be respectful to each other.</strong></h3><br/>
+                    Treat everyone with respect. Participate while acknowledging that everyone deserves to be here — and each of 
+                    us has the right to enjoy our experience without fear of harassment, discrimination, or condescension, whether 
+                    blatant or via micro-aggressions. All forms of communication should not demean others. Consider what you are 
+                    saying and how it would feel if it were said to you, or about you. <br/>
+                </p>
+            </div>
 
-        <div>
-        Participants asked to stop any harassing behavior are expected to comply immediately. Our zero-tolerance 
-        policy means that we will look into and review every allegation of violation of our Community Guidelines 
-        and Anti-Harassment Policy and respond appropriately. to report any behavior that makes you or others 
-        feel uncomfortable, send us a note at W0455841@nscc.ca <br/>
-        This policy extends to talks, forums, workshops, social media, partners, sponsors, staff, etc. 
-        You catch our drift. Poop Logger reserves the right to refuse admittance to, or remove any person from, 
-        any Poop Logger forum at any time in its sole discretion. This includes, but is not limited to, 
-        Popp Loggers behaving in a disorderly manner or failing to comply with this policy, and the terms 
-        and conditions herein. If a participant engages in harassing or uncomfortable behavior, Poop Logger may 
-        take any action they deem appropriate, including warning or expelling the offender from the app with blocking 
-        the offender's account from participating online. 
-        </div>
 
-    </div>
-    
-  );
+            <div>
+                <p>
+                    <h3>Speak up if you see or hear something. </h3>
+                    Harassment is not tolerated, and you are empowered to politely engage when you or others are disrespected. 
+                    The person making you feel uncomfortable may not be aware of what they are doing, and politely bringing their 
+                    behavior to their attention is encouraged. <br/>
+                </p>           
+            </div>
+
+            <div>
+                <p>
+                    <h2>
+                        We have a <strong>ZERO TOLERANCE POLICY</strong> for online harassment of any kind, including but not limited to:
+                    </h2>
+                    <ul>
+                        <li>Deliberate intimidation</li>
+                        <li>Offensive language </li>
+                        <li>Language that reinforces social structures of domination</li>
+                        <li>Cyber threats </li>
+                        <li>Deliberate intimidation</li>
+                    </ul>
+                </p>         
+            </div>
+
+            <div>
+                <p>
+                    <h2>
+                        In relation to, but not limited to:
+                    </h2>
+                    <ul>
+                        <li>Neurodiversity</li>
+                        <li>Race</li>
+                        <li>Color</li> 
+                        <li>National origin </li>
+                        <li>Gender identity </li>
+                        <li>Gender expression </li>
+                        <li>Sexual orientation </li>
+                        <li>Age</li>
+                        <li>Body size</li>
+                        <li>Disabilities </li>
+                        <li>Appearance </li>
+                        <li>Religion </li>
+                        <li>Pregnancy </li>
+                        <li>Military status </li>
+                        <li>Social demographic</li>
+                    </ul>
+                </p>           
+            </div>
+
+            <div>
+                <p>
+                    Participants asked to stop any harassing behavior are expected to comply immediately. Our zero-tolerance 
+                    policy means that we will look into and review every allegation of violation of our Community Guidelines 
+                    and Anti-Harassment Policy and respond appropriately. to report any behavior that makes you or others 
+                    feel uncomfortable, send us a note at W0455841@nscc.ca <br/>
+                    This policy extends to talks, forums, workshops, social media, partners, sponsors, staff, etc. 
+                    You catch our drift. Poop Logger reserves the right to refuse admittance to, or remove any person from, 
+                    any Poop Logger forum at any time in its sole discretion. This includes, but is not limited to, 
+                    Popp Loggers behaving in a disorderly manner or failing to comply with this policy, and the terms 
+                    and conditions herein. If a participant engages in harassing or uncomfortable behavior, Poop Logger may 
+                    take any action they deem appropriate, including warning or expelling the offender from the app with blocking 
+                    the offender's account from participating online. 
+                </p>
+            </div>
+        </div>    
+    );
 }
 
 export default CommunityGuidelines;
