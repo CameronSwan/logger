c2ac4da9cbd7418ac68b8e6bda8f00e125ade40c
Christina Hemeon <christinahemeon@gmail.com>
2023-03-17 18:09:11 -0300

    Add favicons and styling to header

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  return (
-    <nav>
-      <Link to="/" className="navbar-brand d-flex align-items-center">
-        <strong>Logger</strong>
-      </Link>
-    </nav>
-  );
+	return (
+		<nav className='navbar'>
+			<div className='navbar__logo'>
+				<Link to='/' className='navbar__logo-title logo'>
+					<strong>Logger</strong>
+				</Link>
+			</div>
+		</nav>
+	);
 };
 
 export default NavBar;
