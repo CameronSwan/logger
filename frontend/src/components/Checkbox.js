1fbf6839bacafd753826af120b8ecd30ef360c1b
Christina Hemeon <christinahemeon@gmail.com>
2023-03-17 14:31:06 -0300

    Fix styling on forms

--- frontend/src/components/Checkbox.js
+++ frontend/src/components/Checkbox.js
@@ -10,7 +10,7 @@ const Checkbox = ({ id, label, checked, value, onChange, className, name, title 
 				onChange={onChange}
 				title={title}
 			/>
-			<label htmlFor={id}>{label}</label>
+			<label htmlFor={id} title={title}>{label}</label>
 		</div>
 	);
 };
