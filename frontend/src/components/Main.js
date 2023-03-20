054a7fffbcfce2b206021698c46cb55fd690ba6a
Christina Hemeon <christinahemeon@gmail.com>
2023-03-19 23:16:20 -0300

    Add icons to tabs and buttons

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import history from '../img/icons/history.svg';
+import analysis from '../img/icons/analysis.svg';
+import community from '../img/icons/community.svg';
+import info from '../img/icons/info.svg';
 
 const Main = () => {
 
@@ -7,18 +11,30 @@ const Main = () => {
         <div className='main outside-spacing'>
             <h1 className='underlined bottom-spacing'>Hello</h1>
             <div>
-            <p>Coming soon!</p>
-            <div className='form__button-row'>
-                <Link to='/bowelmovement/create' rel='path' className='button button--link button--submit button__new-entry cta'>New Entry</Link>
+                <p>Coming soon!</p>
+                <div className='form__button-row'>
+                    <Link to='/bowelmovement/create' rel='path' className='button button--submit button--new-entry cta'>New Entry</Link>
+                </div>
             </div>
-            </div>
-           
+
             <h2 className='underlined bottom-spacing'>Explore</h2>
             <div className='main__explore-tabs'>
-                <Link to='/bowelmovement/history' className='tab h2 link'>History</Link>
-                <Link to='/bowelmovement/analysis' className='tab h2 link'>Analysis</Link>
-                <Link to='/community' className='tab h2 link'>Community</Link>
-                <Link to='/info' className='tab h2 link'>Info</Link>
+                <Link to='/bowelmovement/history' className='tab h2 link'>
+                    <img src={history} className='tab__icon' alt=''/>
+                    History
+                </Link>
+                <Link to='/bowelmovement/analysis' className='tab h2 link'>
+                <img src={analysis} className='tab__icon' alt=''/>
+                    Analysis
+                </Link>
+                <Link to='/community' className='tab h2 link'>
+                <img src={community} className='tab__icon' alt=''/>
+                    Community
+                </Link>
+                <Link to='/info' className='tab h2 link'>
+                <img src={info} className='tab__icon' alt=''/>
+                    Info
+                </Link>
             </div>
         </div>
 
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const NavBar = () => {
 	const location = useLocation();
