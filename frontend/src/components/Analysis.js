b3bb5a9bef077c041a06a18c95ce5d8fc7883813
Christina Hemeon <christinahemeon@gmail.com>
2023-03-21 13:06:50 -0300

    Work on gathering data for analysis

--- frontend/src/components/Analysis.js
+++ frontend/src/components/Analysis.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import dataService from '../services/dataService';
 
 const Analysis = () => {
+    const [bowelmovements, setBowelmovements] = useState([]);
+    const [bmDates, setBMDates] = useState([]);
+
+
+    const bmCount = bowelmovements.length
+
+    const bmCountPerDay = bmDates.reduce((bmsByDay, bmDate) => {
+        if (!bmsByDay[bmDate]) {
+            bmsByDay[bmDate] = 0;
+        }
+        bmsByDay[bmDate]++
+        return bmsByDay
+    }, {});
+
+    const bmCountPerDaySorted = Object.entries(bmCountPerDay).sort(([, a], [, b]) => a - b).reverse()
+
+    const bmDaysCount = Object.keys(bmCountPerDay).length;
+
+    const highestBmCountDate = Object.keys(bmCountPerDay).reduce((a, b) => bmCountPerDay[a] > bmCountPerDay[b] ? a : b, {});
+
+    const highestBmCountPerDay = bmCountPerDay[highestBmCountDate]
+
+    console.log(bmCountPerDaySorted)
+    console.log(highestBmCountDate)
+    //console.log(Object.keys(bmCountPerDay).find(day => bmCountPerDay[day] === highestBmCountPerDay))
+
+    console.log(Object.keys(bmCountPerDay).filter(count => bmCountPerDay[count] === highestBmCountPerDay))
+    
+    useEffect(() => {
+        dataService.getBowelMovements(bowelmovements => {
+            setBowelmovements(bowelmovements)
+            setBMDates(bowelmovements.map(bm => bm.date))
+        })
+    }, [])
+
 
     return (
-        <div className='outside-spacing'>
-            <h1 className='underlined'>Analysis</h1>
+        <div className='outside-spacing analysis'>
+            <h1 className='underlined bottom-spacing'>Analysis</h1>
+            <div className='analysis__counts'>
+                <div>
+                    <div>{bmCount}</div>
+                    <span className='label'>Entries Recorded </span>
+                </div>
+                
+                <div>
+                    <div>{bmDaysCount}</div>
+                    <span className='label'>Days Recorded</span>
+                </div>
+            </div>
         </div>
     )
 }
