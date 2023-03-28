9f66019dbf62f9017f16dfe6194a1a27982c633c
Cameron Swan <swanc@dal.ca>
2023-03-28 11:16:31 -0300

    commit

--- backend/routes/api/facts.js
+++ backend/routes/api/facts.js
@@ -25,17 +25,4 @@ router.get('/:id', (req, res) => {
     })
 })
 
-/**
- * Retrive Random Fact.
- */
-router.get('/random', (req, res) => {
-    Fact.find({}, (e, facts) => {
-        if (e) res.status(500).send({ serverMessage: "An Error Occured."})
-        else {
-            if (facts.length === 0) res.status(404).send({ serverMessage: "Could Not Find Any Facts :("})
-            else res.status(200).send(facts[Math.random() * facts.length])
-        }
-    })
-})
-
 module.exports = router
