aed9110f56e829de7cbb8978fcee5ed43b62d8f2
Christina Hemeon <christinahemeon@gmail.com>
2023-03-30 08:39:58 -0300

    Add navbar items

--- frontend/src/components/ForgotLogin.js
+++ frontend/src/components/ForgotLogin.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 
 // TODO: Set up email to direct users
 const ForgotLogin = () => {
-
+    const [email, setEmail] = useState('');
     const [message, setMessage] = useState([]);
 
+    const handleSetMessage = () => {
+        email ? setMessage('Check your email for instructions on how to reset your password') : setMessage('Please submit an email address')
+    }
 
     return (
         <div className='outside-spacing'>
@@ -21,16 +24,18 @@ const ForgotLogin = () => {
                         name='email'
                         placeholder='Email address'
                         className='form__text-input'
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                     />
                 </div>
                 <div className='form__error form__error--final'>
                     {
-                        message && <span className='error form__row'>{message}<br/></span>
+                        message && <span className='error'>{message}<br /></span>
                     }
                 </div>
                 <div className='form__button-row'>
-                    <button type='button' className='button button--submit cta'  onClick={() => setMessage('Check your email for your new password.')}>
-                        Log In
+                    <button type='button' className='button button--submit cta' onClick={handleSetMessage}>
+                        Submit
                     </button>
                 </div>
 
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import authService from '../services/authService';
 import navIcon from '../img/icons/nav-icon.svg';
+import navIconClose from '../img/icons/nav-icon-close.svg';
+import back from '../img/icons/back.svg';
 
 const NavBar = () => {
+	const [dropdownOpen, setDropDownOpen] = useState(false);
 	const location = useLocation();
 	const navigate = useNavigate();
+	const navbarClassName = location.pathname === '/login' ? 'navbar--hidden' : 'navbar';
+	const dropdownClassName = dropdownOpen ? 'navbar__dropdown' : 'navbar__dropdown navbar__dropdown--hidden'
+
+	const handleDropdown = () => {
+		setDropDownOpen(!dropdownOpen)
+
+	}
 
 	const logOut = () => {
-		authService.signout();
+		authService.logout();
 		navigate('/login');
 	}
 
-	let className = location.pathname === '/login' ? 'navbar--hidden' : 'navbar';
-
 	return (
-		<nav className={className}>
+		<nav className={navbarClassName}>
 			{location.pathname !== '/login' &&
 				<div className='navbar__content'>
 					<div>back</div>
@@ -25,8 +33,32 @@ const NavBar = () => {
 						</Link>
 					</div>
 
-					<button className='button--link link' tyoe='button' aria-label='menu'><img src={navIcon} /></button>
-					{/* <div className='navbar__dropdown'></div> */}
+					<button className='button--link link' type='button' aria-label='dropdown menu' onClick={handleDropdown}><img src={dropdownOpen ? navIconClose : navIcon} className=' navbar__icon' alt='menu icon' /></button>
+					<div className={dropdownClassName}>
+						<div onClick={handleDropdown}>
+							{
+								!authService.isAuthenticated() && 
+								<div className='navbar__item'>
+									<Link to='/login' className='navbar__link button--link link' relative='path'>Log in</Link>
+								</div>
+							}
+							{
+								!authService.isAuthenticated() && 
+								<div className='navbar__item'>
+									<Link to='/register' className='navbar__link button--link link' relative='path'>Register</Link>
+								</div>
+							}
+							<div className='navbar__item'>
+								<Link to='/about' className='navbar__link button--link link' relative='path'>About</Link>
+							</div>
+							{
+								authService.isAuthenticated() &&
+								<div className='navbar__item'>
+									<button className='navbar__link button--link link' onClick={logOut}>Sign Out</button>
+								</div>
+							}
+						</div>
+					</div>
 				</div>}
 		</nav>
 	);
